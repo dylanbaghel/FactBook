aa7281a12c38e4eed578e4c4b82f24630cec3170
baghelabhi39@gmail.com <baghelabhi39@gmail.com>
2017-10-18 08:21:27 -0700

    Created a randomFact Return Method that will give a random facts

--- FactBook/FactBook/FactWheel.cs
+++ FactBook/FactBook/FactWheel.cs
@@ -8,15 +8,15 @@ namespace FactBook
 {
     class FactWheel
     {
-        private String[] factBook = {"Abhishek Baghel is Superman",
+        private String[] facts = {"Abhishek Baghel is Superman",
         "Honey Singh is SuperStar!",
         "Dylan Bahgel uses Github repository for his projects",
         "Why ASP.Net is Better than PHP.",
         "PYTHON is a programming language and a snake also"};
 
         public String randomFact() {
-            int randomNumber = new Random().Next(1,factBook.Length);
-            return factBook[randomNumber];
+            int randomNumber = new Random().Next(1,facts.Length);
+            return facts[randomNumber];
         }
     }
 }
